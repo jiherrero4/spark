c478445b2e37051a037473c62758f2f092318493
jiherrero4 <jiherrero4@gmail.com>
2016-07-23 12:49:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def processRequest(req):
     yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
     result = urllib.urlopen(yql_url).read()
     data = json.loads(result)
-    createRoom()
     res = makeWebhookResult(data)
     return res
 
@@ -92,18 +91,6 @@ def makeWebhookResult(data):
         "source": "apiai-weather-webhook-sample"
     }
     
-def createRoom:
-    # Define header used for authentication
-    myToken="NWNmODg2ODAtZDVmMC00MjhmLThhN2YtYzJhYWIxODQwZDkwNDVkMjYzMzYtYTU1"
-    roomTitle="PruebaCreacionSala"
-    headers = { "Authorization": "Bearer "+myToken,   "Content-type": "application/json" }
-    # Define the action to be taken in the HTTP request 
-    roomInfo = { "title": roomTitle }
-    # Execute HTTP POST request to create the Spark Room 
-    r = requests.post("https://api.ciscospark.com/v1/rooms",headers=headers, json=roomInfo)
-    room = r.json()
-    # Print the result of the HTTP POST request
-    print(room)
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
